8b300bd70d29faf92d688fe7db49997a24a7f3ad
steve donovan <steve.j.donovan@gmail.com>
2013-10-14 18:18:50 +0200

    forgot to add new files to makefile

--- llib/build.bat
+++ llib/build.bat
@@ -1,8 +1,10 @@
-cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   obj.c /Foobj.obj
-cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   sort.c /Fosort.obj
-cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   list.c /Folist.obj
-cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   file.c /Fofile.obj
-cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   scan.c /Foscan.obj
-cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   map.c /Fomap.obj
-cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   str.c /Fostr.obj
-lib /nologo obj.obj sort.obj list.obj file.obj scan.obj map.obj str.obj /OUT:llib_static.lib
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   obj.c
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   sort.c
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   list.c
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   file.c
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   scan.c
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   map.c
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   str.c
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   template.c
+cl /nologo -c /O2 /WX /TP /D_CRT_SECURE_NO_DEPRECATE /DNDEBUG   value.c
+lib /nologo obj.obj sort.obj list.obj file.obj scan.obj map.obj str.obj template.obj value.obj /OUT:llib_static.lib
