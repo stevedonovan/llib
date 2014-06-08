61b5665f3c6fd0188c69bab153726f308cebea5b
steve donovan <steve.j.donovan@gmail.com>
2014-06-08 19:34:29 +0200

    some useful map/filter array macros

---
+++ llib/array.h
@@ -0,0 +1,68 @@
+/*
+* llib little C library
+* BSD licence
+* Copyright Steve Donovan, 2013
+*/
+
+/***
+### Useful Array Macros
+
+These operate on llib arrays, supporting mapping, filtering and inline iteration.
+With all these macros, the dummy variable for accessing the current array
+value is `_` (underscore).  The map & filter macros all declare an array of
+the given type, and create a reference-counted array if the name ends in 'R'.
+*/
+
+#ifndef _LLIB_ARRAY_H
+#define _LLIB_ARRAY_H
+
+#include "obj.h"
+
+#ifdef __cplusplus
+#define _T_ decltype
+#else
+#define _T_ __typeof__
+#endif
+
+/// Iterate over an array.
+// @param arr the array (can be an expression)
+// @param expr the expression containing `_`
+// @macro FORA
+// @usage FOR(ints,printf("%i ",_));
+#define FORA(A_,E_) {_T_(A_) a_ = A_; FOR(i,array_len(a_)) \
+  { _T_(*a_) _ = a_[i]; E_; }}
+
+/// Declare an array which is `[F(x)|x in A]`
+// @param T the resulting type
+// @param v the variable name
+// @param F the expression containing `_`
+// @param A the source array
+// @macro MAPA
+// @usage MAPA(int,ints,ceil(_),floats);
+#define MAPA(T_,v_,E_,A_) T_ *v_; {_T_(A_) a_ = A_; v_ = array_new(T_,array_len(a_)); \
+ FOR(i_,array_len(a_)) { _T_(*a_) _ = a_[i_]; v_[i_] = E_; } }
+
+/// Map an expression over an array
+// @param T the resulting type
+// @param v the variable name
+// @param expr the expression containing `_`
+// @param arr the source array
+// @macro MAPAR
+// @usage MAPAR(char*,strs,str_fmt("%d",_),ints);
+#define MAPAR(T_,v_,E_,A_) T_ *v_; {_T_(A_) a_ = A_; v_ = array_new_ref(T_,array_len(a_)); \
+ FOR(i_,array_len(a_)) { _T_(*a_) _ = a_[i_]; v_[i_] = E_; } }
+
+/// New array containing matching elements from another.
+// @param T array type
+// @param v the variable name
+// @param arr the input array
+// @param expr the filter expression in `_`
+#define FILTA(T_,v_,A_,E_) T_ *v_; {_T_(A_) a_ = A_; T_**s_ = seq_new(T_); \
+ FOR(i_,array_len(a_)) { _T_(*a_) _ = a_[i_]; if (E_) seq_add(s_,_); } \
+ v_ = (T_*)seq_array_ref(s_); }
+ 
+ #define FILTAR(T_,v_,A_,E_) T_ *v_; {_T_(A_) a_ = A_; T_**s_ = seq_new_ref(T_); \
+ FOR(i_,array_len(a_)) { _T_(*a_) _ = a_[i_]; if (E_) seq_add(s_,obj_ref(_)); } \
+ v_ = (T_*)seq_array_ref(s_); }
+
+#endif
