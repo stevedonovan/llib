6070c9bdc6071fa456dd41d5939bcfc260897196
steve donovan <steve.j.donovan@gmail.com>
2015-04-25 09:03:16 +0200

    all.h has most of the llib headers included

---
+++ llib/all.h
@@ -0,0 +1,13 @@
+#ifndef __LLIB_ALL_H
+#define __LLIB_ALL_H
+#include <llib/str.h>
+#include <llib/file.h>
+#include <llib/value.h>
+#include <llib/array.h>
+#include <llib/json.h>
+#include <llib/xml.h>
+#include <llib/table.h>
+#include <llib/config.h>
+#include <llib/farr.h>
+#include <llib/scan.h>
+#endif
